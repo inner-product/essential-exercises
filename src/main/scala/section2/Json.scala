2d60f3eaa095209d83016032b9d2ce9dda11058b
Noel Welsh <noel@untyped.com>
2021-04-14 19:13:06 +0100

    Add JSON exercise

---
+++ src/main/scala/section2/Json.scala
@@ -0,0 +1,13 @@
+package essentialscala.section2
+
+// Your mission is to implement a data type to represent JSON.
+//
+// If you know about the built-in collection types such as List and Map it's ok
+// to use them as part of your model.
+//
+// Need some help? At json.org you'll find a "railway diagram" that defines the
+// syntax of JSON. If you look past the syntax you'll find the data model is
+// described there as well.
+//
+// If you complete this, and still have time, implement a method called `print`
+// that prints your JSON data structure to a `String` as valid (textual) JSON.
