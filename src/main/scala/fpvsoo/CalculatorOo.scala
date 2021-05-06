a62935e1eba68ad550a4ec83416757b17b057288
Noel Welsh <noel@noelwelsh.com>
2021-05-06 20:14:51 +0100

    Add FP vs OO exercise

---
+++ src/main/scala/fpvsoo/CalculatorOo.scala
@@ -0,0 +1,20 @@
+package fpvsoo
+
+// Your missions:
+//
+// Mission One:
+//
+// Define an interface to represent arithmetric expressions, with the following methods:
+//
+// - literal, which accepts a Double and returns a Double
+// - add, which accepts two Doubles and returns a Double
+// - subtract, which accepts two Doubles and returns a Double
+// - divide, which accepts two Doubles and returns a Double
+// - multiply, which accepts two Doubles and returns a Double
+//
+//
+// Mission Two:
+//
+// Define an interface that extends the interface you defined above and adds
+// trigonometric functions: sin and cos.
+object CalculatorOo
---
+++ src/main/scala/fpvsoo/README.md
@@ -0,0 +1,3 @@
+# FP vs OO
+
+In this exercise we investigate the relationship between FP and OO code, and learn how we can convert between the two.
