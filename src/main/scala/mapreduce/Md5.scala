3fe2180fc3ba39634de1e9c90d62123ab6b84977
Noel Welsh <noel@noelwelsh.com>
2021-05-06 17:05:20 +0100

    Make md5 thread safe
    The MessageDigest class is not thread-safe, so reusing the same instance
    across multiple threads can cause a crash. Make it thread-safe by
    creating a new instance each time `digest` is called.

--- src/main/scala/mapreduce/Md5.scala
+++ src/main/scala/mapreduce/Md5.scala
@@ -3,7 +3,7 @@ package mapreduce
 import java.security.MessageDigest
 
 object Md5 {
-  val digest = MessageDigest.getInstance("MD5")
+  def digest = MessageDigest.getInstance("MD5")
 
   def md5(str: String): String =
     digest.digest(str.getBytes).map("%02x".format(_)).mkString
