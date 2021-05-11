77aacc88262675a65b2c920e153f48ff3671d316
Noel Welsh <noel@noelwelsh.com>
2021-05-11 16:04:05 +0100

    Implement solutions to cats exercises

--- src/main/scala/felines/Lists.scala
+++ src/main/scala/felines/Lists.scala
@@ -6,18 +6,18 @@ object Lists {
   // If all the options contain values return a Some containing a list of values.
   // Otherwise return None.
   def listOfOptionToOptionOfList(l: List[Option[Int]]): Option[List[Int]] =
-    ???
+    l.sequence
 
   // Sum all the values within the options, returning a Some containing the sum
   // or None if all the options are None.
   def addOptionList(l: List[Option[Int]]): Option[Int] =
-    ???
+    l.combineAll
 
   // Given a list of A, and function from A => Option[B], if all the results of
   // calling the function are Some return a Some containing a list of values.
   // Otherwise return None.
   def listErrorHandling[A, B](l: List[A], f: A => Option[B]): Option[List[B]] =
-    ???
+    l.traverse(f)
 
   // This line uses Cats implicits, and hence prevents the compiler complaining
   // about an unused import before the methods above have been implemented.
--- src/main/scala/felines/Options.scala
+++ src/main/scala/felines/Options.scala
@@ -11,18 +11,18 @@ object Options {
   //   addIntOption(Some(1), Some(3)) == Some(4)
   //   addIntOption(Some(1), None)    == Some(1)
   def addIntOption(a: Option[Int], b: Option[Int]): Option[Int] =
-    ???
+    a |+| b
 
   // If both options have values return a tuple of the values. Otherwise return None.
   def tupleOption(a: Option[Int], b: Option[String]): Option[(Int, String)] =
-    ???
+    (a, b).tupled
 
   // If both options contain values, append the two lists. Otherwise return None.
   def appendListOption(
       a: Option[List[String]],
       b: Option[List[String]]
   ): Option[List[String]] =
-    ???
+    (a, b).mapN((l1, l2) => l1 ++ l2)
 
   // If both options contain a value, return an option containing the String
   // "Hi" repeated the number of times equals to the product of both values.
@@ -33,12 +33,12 @@ object Options {
   //
   // Pro tip: There is a multiply method on String: "Hi" * 2 == "HiHi"
   def multiplyIntOption(a: Option[Int], b: Option[Int]): Option[String] =
-    ???
+    (a, b).mapN((i1, i2) => "Hi" * i1 * i2)
 
   // If both or either option contains a value return the sum of all the Ints
   // Other return None.
   def sumListOption(a: Option[List[Int]], b: Option[List[Int]]): Option[Int] =
-    ???
+    (a |+| b).map(_.sum)
 
   // This line uses Cats implicits, and hence prevents the compiler complaining
   // about an unused import before the methods above have been implemented.
