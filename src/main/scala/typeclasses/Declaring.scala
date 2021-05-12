da4206311286db2874ab9b8184472e1d316ccf05
Noel Welsh <noel@untyped.com>
2021-05-12 19:23:46 +0100

    Add type class exercises

---
+++ src/main/scala/typeclasses/Declaring.scala
@@ -0,0 +1,33 @@
+package typeclasses
+
+
+object Declaring {
+  // A Configuration value of type A is
+  // - A Value containing a value of type A OR
+  // - Default
+  //
+  // This is an example of what pattern?
+  // Implement it in Scala.
+
+  // Configuration can have some fun methods. Implement the below:
+  // - map
+  // - flatMap
+
+  // Implement a Monoid instance for Configuration. If two Configurations are a
+  // Value the right one should be chosen (so that later configuration overrides
+  // earlier configuration). If one is Default and the other a Value the result
+  // should be the Value. In other words, Default is the identity for
+  // Configuration.
+
+  // Implement a Monad instance for Configuration.
+
+  // Having implemented a Monad instance do you need to implement a Semigroupal
+  // instance?
+
+  // Optional: Configuration has the same structure (is isomorphic to) a type in the
+  // standard library. What type is that?
+
+  // Optional: implement a Parallel instance for Configuration, so that parMapN
+  // and friends do something useful.
+
+}
---
+++ src/main/scala/typeclasses/Using.scala
@@ -0,0 +1,33 @@
+package typeclasses
+
+import cats._
+
+object Using {
+  // Complete the definition of this method. It should combine the given values
+  // with the given Semigroup instance.
+  def combine[A](a1: A, a2: A)(implicit m: Semigroup[A]): A =
+    ???
+
+  // Complete the definition of this method. It should combine all the values in
+  // the list with a Monoid instance for A. (Why a Monoid instance and not a
+  // Semigroup instance?)
+  def combineAll[A](l: List[A]): A =
+    ???
+
+  // Complete the definition of this method. It should combine all the values in
+  // the list with a Semigroup instance for A and return them wrapped in Some.
+  // If there are no values in the list it should return None.
+  def combineAllOpt[A](l: List[A]): Option[A] =
+    ???
+
+  // Complete the definition of this method. It should combine all the values in
+  // the container A, which should have a Foldable instance, with a Monoid
+  // instance for A.
+  def combineAllF[F[_], A](fa: F[A]): A =
+    ???
+
+
+  // If you used a implicit parameter in combineAll, combineAllOpt, and
+  // combineAllF above, rewrite them using a context bound. If you used a
+  // context bound do the reverse.
+}
