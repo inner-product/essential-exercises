4d692c494a6c3ed0636772c94c14328fcf7fff84
Noel Welsh <noel@noelwelsh.com>
2021-05-25 14:44:08 +0100

    Simple ADT exercises

---
+++ src/main/scala/adt/FingerExercises.scala
@@ -0,0 +1,47 @@
+package adt
+
+// Each of these problem is a simple exercise using algebraic data types. This
+// make good exercises to get used to the syntax.
+object FingerExercises {
+  // A Continent is
+  // - Asia;
+  // - Africa;
+  // - North America;
+  // - South America;
+  // - Antartica;
+  // - Europe; or
+  // - Australia
+  //
+  // Represent this in code
+
+  // A Hotel booking is
+  // - a Hotel Name (String);
+  // - an Address (String);
+  // - a Check-in Date (java.time.LocalDate); and
+  // - a Check-out Date (java.time.LocalDate)
+  //
+  // Represent this in code
+
+  // A Canis, the genus that includes dogs, is be one of seven species:
+  // - Side-striped Jackal;
+  // - Golden Jackal;
+  // - Coyote;
+  // - Gray Wolf;
+  // - Black-backed Jackal;
+  // - Red Wolf; or
+  // - Ethiopian Wolf
+  //
+  // Represent this in code
+
+  // A BlogPost is
+  // - a Title (String);
+  // - a Body (String);
+  // - an Author (String); and
+  // - a State (see below)
+  //
+  // A State is
+  // - Draft; or
+  // - Published, which has a publication data (java.time.LocalDateTime)
+  //
+  // Represent this in code
+}
