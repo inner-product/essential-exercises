8b0f7b418b92c4e788b98a0f5ab1cb43692aee1a
Noel Welsh <noel@noelwelsh.com>
2021-06-18 16:01:14 +0100

    Add exercise for typeclass based Json serialization

---
+++ src/main/scala/json/Json.scala
@@ -0,0 +1,22 @@
+package json
+
+// Representation of Json as an algebraic data type
+sealed trait Json
+final case class JObject(fields: Map[String, Json]) extends Json
+final case class JArray(values: List[Json]) extends Json
+final case class JNumber(value: Double) extends Json
+final case class JString(value: String) extends Json
+final case class JBoolean(value: Boolean) extends Json
+case object JNull extends Json
+
+// Type class
+trait JsonWriter[A] {
+  def write(a: A): Json
+}
+
+// Implement type class instances to write as Json
+// - String
+// - Boolean
+// - a List of any type that can be written as Json
+//   I.e. List[A] where there is a JsonWriter[A]
+// - an Option of any type that can be written as Json
