f8aee04f1cab654b750a1f98b016f3f8fb591a78
Noel Welsh <noel@noelwelsh.com>
2021-11-05 14:35:39 +0000

    Clarify calculator exercise

--- src/main/scala/adt/Calculator.scala
+++ src/main/scala/adt/Calculator.scala
@@ -19,13 +19,14 @@ package adt
 // will take an expression and produce a Double using the usual rules of
 // mathematics. The second interpreter will take an expression and produce a
 // String representing that expression, with correct parentheses to indicate
-// precedence.
+// precedence. Implement these interpreters in methods that are *not* on the
+// Expression type. I.e. create a new object a put them there.
 //
 // Learning points:
 // - Appreciate the utility of algebraic data types and structural recursion. Most
 // of the code should follow from the programming strategies.
 //
 // - Understand how representing the expressions as data ("reifying" them)
-// decouples the data representation for the functions that act on it, and
+// decouples the data representation from the functions that act on it, and
 // allows us to implement different interpreters.
 object Calculator
