595986216f1f07b406b8d80ec37f0cc483d4daf7
Noel Welsh <noel@noelwelsh.com>
2021-11-09 14:16:28 +0000

    Improve wording

--- src/main/scala/generics/Maybe.scala
+++ src/main/scala/generics/Maybe.scala
@@ -1,12 +1,16 @@
-package sequencing
+package generics
 
 // In this exercise we'll implement the equivalent of the built-in Option.
 //
-sealed trait Maybe[A] {
-  def contains(x: A): Boolean =
+// Maybe is Just or Empty
+// Just is a value of type A
+// What strategy / concept is Maybe an example of?
+sealed trait Maybe[+A]
+{
+  def contains[AA >: A](x: AA): Boolean =
     ???
 
-  def getOrElse(default: A): A =
+  def getOrElse[AA >: A](default: AA): A =
     ???
 
   def map[B](f: A => B): Maybe[B] =
@@ -19,8 +23,11 @@ sealed trait Maybe[A] {
     ???
 }
 object Maybe {
-  def empty[A]: Maybe[A] = ???
+  final case class Just[A](value: A) extends Maybe[A]
+  final case object Empty extends Maybe[Nothing]
+  //final case class Empty[A]() extends Maybe[A]
 
-  def apply[A](x: A): Maybe[A] =
-    ???
+  def empty[A]: Maybe[A] = Empty
+
+  def apply[A](x: A): Maybe[A] = Just(x)
 }
