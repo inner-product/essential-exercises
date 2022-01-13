536c989e5c3309c6f925965725cc9b108441d80e
Noel Welsh <noel@noelwelsh.com>
2022-01-13 18:12:05 +0000

    maxOption is simpler to implement than max
    Total function vs partial function

--- src/main/scala/implicits/MyList.scala
+++ src/main/scala/implicits/MyList.scala
@@ -1,10 +1,10 @@
 package implicits
 
 sealed trait MyList[A] {
-  // Implement max
+  // Implement maxOption
   // See the documentation for the same method on List for details
   // https://www.scala-lang.org/api/current/scala/collection/immutable/List.html
-  def max: A = ???
+  def maxOption: Option[A] = ???
 }
 object MyList {
   final case class Pair[A](head: A, tail: MyList[A]) extends MyList[A]
@@ -18,8 +18,8 @@ object MyList {
 
 object MyListExample extends App {
   val intList = MyList(1, 2, 3, 4)
-  println(s"intList.max is ${intList.max}, should be 4")
+  println(s"intList.max is ${intList.maxOption}, should be Some(4)")
 
   val doubleList = MyList(3.0, 10.0, 2.0, -3.0)
-  println(s"doubleList.max is ${doubleList.max}, should be 10.0")
+  println(s"doubleList.max is ${doubleList.maxOption}, should be Some(10.0)")
 }
